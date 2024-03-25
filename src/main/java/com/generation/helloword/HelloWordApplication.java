d357478c10bac77e06c68b913c7b3e08d957df84
Alan <alan-hd@hotmail.com>
2024-03-25 13:26:10 -0300

    First commit

---
+++ src/main/java/com/generation/helloword/HelloWordApplication.java
@@ -0,0 +1,22 @@
+package com.generation.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordApplication.class, args);	
+		
+
+	
+	}
+	
+	public class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World!");
+	    }
+	}
+
+}
